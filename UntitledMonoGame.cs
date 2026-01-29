ccc422506ecccdefc3ce9c528678837bdd0315d0
Gabriel G. Cunha <gabrielmesmo97@gmail.com>
2026-01-29 09:19:19 -0300

    Add music and sounds

--- UntitledMonoGame.cs
+++ UntitledMonoGame.cs
@@ -6,6 +6,8 @@ using LemonicLib;
 using LemonicLib.Graphics;
 using LemonicLib.Input;
 using System;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Audio;
 
 namespace UntitiledMonoGame;
 
@@ -22,11 +24,15 @@ public class UntitledMonoGame : Core
     private Texture2D _smileyWalkTetxure;
     AnimatedSprite _smileyWalk;
 
+    private Song _linkinParkInTheEnd;
+    private SoundEffect _soundEffect;
+
     private float _smileySpeed = 240f;
     private float _smileyX;
     private float _smileyY;
 
     private bool _mouseControl = false;
+    private string _controlText = "Keyboard/GamePad Control";
 
     public UntitledMonoGame() : base("Untitled", 800, 480, false)
     {
@@ -44,6 +50,8 @@ public class UntitledMonoGame : Core
     protected override void LoadContent()
     {
         _font = Content.Load<SpriteFont>("message_font");
+        _linkinParkInTheEnd = Content.Load<Song>("audio/linkin-park_in-the-end");
+        _soundEffect = Content.Load<SoundEffect>("audio/tx0_fire1");
 
         _tilesetTexture = Content.Load<Texture2D>("textures/tileset");
         _tileset = new SpriteRegion(_tilesetTexture, new Rectangle(128, 0, 32, 32));
@@ -59,6 +67,14 @@ public class UntitledMonoGame : Core
     {
         float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
 
+        // Play Linkin Park
+        if (MediaPlayer.State != MediaState.Playing)
+        {
+            MediaPlayer.Play(_linkinParkInTheEnd);
+            MediaPlayer.Volume = 0.6f;
+        }
+
+
         _smileyWalk.Update(dt);
 
         GamePadInfo gamePadOne = Input.GamePads[(int)PlayerIndex.One];
@@ -66,16 +82,31 @@ public class UntitledMonoGame : Core
         if (Input.Keyboard.IsKeyPressed(Keys.V))
         {
             _mouseControl = !_mouseControl;
+
+            if (_mouseControl)
+            {
+                _controlText = "Mouse Control";
+            }
+            else
+            {
+                _controlText = "Keyboard/GamePad Control";
+            }
         }
+
+        bool action = false;
 
         if (_mouseControl)
         {
             _smileyX = Input.Mouse.X;
             _smileyY = Input.Mouse.Y;
+
+            action = Input.Mouse.IsButtonPressed(MouseButton.Left);
         }
         else
         {
-            if (gamePadOne.IsButtonPressed(Buttons.A))
+            action = Input.Keyboard.IsKeyPressed(Keys.Z) || gamePadOne.IsButtonPressed(Buttons.A);
+
+            if (gamePadOne.IsButtonPressed(Buttons.B))
             {
                 gamePadOne.SetVibration(0.5f, TimeSpan.FromSeconds(1));
             }
@@ -111,6 +142,11 @@ public class UntitledMonoGame : Core
             }
         }
 
+        if (action)
+        {
+            _soundEffect.Play();
+        }
+
         _smileyX = MathHelper.Clamp(_smileyX, 0, 800 - 64);
         _smileyY = MathHelper.Clamp(_smileyY, 0, 480 - 64);
 
@@ -125,7 +161,7 @@ public class UntitledMonoGame : Core
 
         _background.Draw(SpriteBatch, new Rectangle(0, 0, 800, 480), Color.White);
         _tileset.Draw(SpriteBatch, new Rectangle(64, 64, 32, 32), Color.White);
-        SpriteBatch.DrawString(_font, "Hello, world", new Vector2(12, 8), Color.Black);
+        SpriteBatch.DrawString(_font, _controlText, new Vector2(12, 8), Color.Black);
 
         _smileyWalk.Draw(SpriteBatch, new Rectangle((int)_smileyX, (int)_smileyY, 64, 64), Color.White);
 
